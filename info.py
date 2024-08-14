db5cb8a9a91adab118024255e75108f49e60a776
balura45 <112008709+balura45@users.noreply.github.com>
2024-08-14 13:52:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mbg766203:mbg766203@cluster0.jksyr8r.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mbg766203:mbg766203@cluster0.5vusa9z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "AUTO-FILT")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
